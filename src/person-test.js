02524028df509f920879933fa1b950ae5e7b69ab
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-17 12:01:34 -0600

    Remove the skips

--- src/person-test.js
+++ src/person-test.js
@@ -4,33 +4,30 @@ var assert = require('chai').assert;
 
 describe("Person", function() {
 
-  it.skip('should have a name and a dog', function() {
+  it('should have a name and a dog', function() {
     var luna = new Dog({ name: 'Luna', age: 3 });
     var trisha = new Person('Trisha', luna);
-
     assert.equal(trisha.name, 'Trisha');
     assert.instanceOf(trisha.dog, Dog);
+    console.log('trisha.dog.name:', trisha.dog.name);
     assert.equal(trisha.dog.name, 'Luna');
   })
 
-  it.skip('should be able to have no dog', function() {
+  it('should be able to have no dog', function() {
     var dawn = new Person('Dawn');
 
     assert.equal(dawn.dog, undefined);
   })
 
-  it.skip('should feed dog', function() {
+  it('should feed dog', function() {
     var toph = new Dog({ name: 'Toph', age: 2.5});
     var nick = new Person('Nick', toph);
-
     assert.equal(toph.hungry, true);
-
     nick.fillFoodBowl();
-
     assert.equal(toph.hungry, false);
   })
 
-  it.skip('should play with dog', function() {
+  it('should play with dog', function() {
     var wren = new Dog({ name: 'Wren', age: 5});
     var leah = new Person('Leah', wren);
 
@@ -39,7 +36,7 @@ describe("Person", function() {
     assert.equal(pupPlayTime, 'Wren loves playing fetch!');
   })
 
-  it.skip('should notice when dog no longer wants to play', function() {
+  it('should notice when dog no longer wants to play', function() {
     var connie = new Dog({ name: 'Conrad', age: 0.5});
     var kari = new Person('Kari', connie);
 
@@ -53,7 +50,7 @@ describe("Person", function() {
     assert.equal(thirdThrow, 'Oh, I think Conrad is too tired. I guess we won\'t play fetch right now.');
   })
 
-  it.skip('should be able to adopt a dog if they don\'t already have one', function() {
+  it('should be able to adopt a dog if they don\'t already have one', function() {
     var dawn = new Person('Dawn');
 
     dawn.adoptAPup('Rocky', 2);
@@ -63,12 +60,12 @@ describe("Person", function() {
     assert.equal(dawn.dog.age, 2);
   })
 
-  it.skip('should not be able to adopt a dog if they already have one', function() {
+  it('should not be able to adopt a dog if they already have one', function() {
     var cap = new Dog({ name: 'Cap', age: 0.5});
     var kayla = new Person('Kayla', cap);
 
     var adoptionAttempt = kayla.adoptAPup('Chloe', 1);
-
+   
     assert.equal(kayla.dog.name, 'Cap');
     assert.equal(adoptionAttempt, 'You can\'t adopt Chloe. You already have Cap!');
   })
